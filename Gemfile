7b4862ea1a99ec021ba1402d9344e9b8365ec4c3
Sean Callan <seancallan@gmail.com>
2016-03-05 13:28:50 -0800

    Add pagination

--- Gemfile
+++ Gemfile
@@ -1,11 +1,13 @@
 source 'https://rubygems.org'
 
 gem 'activerecord'
+gem 'api-pagination'
 gem 'pg'
 gem 'rake'
 gem 'sinatra', require: 'sinatra/base'
 gem 'sinatra-activerecord'
 gem 'unicorn'
+gem 'will_paginate'
 
 group :test do
   gem 'database_cleaner'
--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'will_paginate'
+require 'will_paginate/active_record'
 
 require_relative 'listing.rb'
 
@@ -14,8 +16,40 @@ class App < Sinatra::Base
 
   get '/listings' do
     listings = lookup(params)
-
+    paginate(listings)
     geo_json(listings)
+  end
+
+  def paginate(listings)
+    curr = params[:page] ||= 1
+
+    response.headers['Total'] = listings.total_entries.to_s
+    response.headers['Per-Page'] = "100"
+    response.headers['Page'] = curr.to_s
+    response.headers['Link'] = page_links(curr, listings)
+  end
+
+  def page_links(curr, listings)
+    curr = curr.to_i
+    last = listings.total_entries / 100
+
+    arr = [page_link(1, 'first')]
+    arr << page_link(curr + 1, 'next') unless curr == last
+    arr << page_link(curr - 1, 'prev') unless curr == 1
+    arr << page_link(last, 'last')
+
+    arr.join(', ')
+  end
+
+  def page_link(page, rel)
+   %Q(<#{uri(page)}>; rel="#{rel}")
+  end
+
+  def uri(page = nil)
+    query = params.merge("page" => page).to_query
+    URI::HTTP.build(host: ENV['HOST'],
+                    path: '/listings',
+                    query: query)
   end
 
   def geo_json(listings)
@@ -37,6 +71,6 @@ class App < Sinatra::Base
     query = query.where('bathrooms <= ?', params[:max_bath]) if params[:max_bath]
     query = query.where('bathrooms >= ?', params[:min_bath]) if params[:min_bath]
 
-    query.all
+    query.paginate(page: params[:page], per_page: 100)
   end
 end
